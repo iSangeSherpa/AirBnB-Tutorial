4673bb2e87ae2b325444f04ccedac901d2037bc0
iSangeSherpa <sangesherpa1215@gmail.com>
2024-01-11 13:40:12 +0545

    Initial Commit

---
+++ AirBnB-Tutorial/AirBnB_TutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirBnB_TutorialApp.swift
+//  AirBnB-Tutorial
+//
+//  Created by Web and App on 11/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct AirBnB_TutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
